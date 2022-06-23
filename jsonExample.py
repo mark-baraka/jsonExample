ef8a32e58a7a720c7a0c3d30bc9280a9584ccbad
Mark.designer <105703138+mark-baraka@users.noreply.github.com>
2022-06-24 00:06:31 +0300

    Add files via upload

---
+++ jsonExample.py
@@ -0,0 +1,7 @@
+#how to use jsonify for dictionary objects
+from flask import jsonify
+@app.route('/person/')
+def hello():
+    return jsonify({'name':'Jimit',
+                    'address':'Kenya'})
+    pass 
